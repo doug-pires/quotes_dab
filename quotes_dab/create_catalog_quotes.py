4d0003da3f37b6bce4cbc4048154346054c32c6f
doug-pires <dpiresmartins@yahoo.com.br>
2023-11-06 10:50:20 +0000

    refactor: Adding path via pathlib

--- quotes_dab/create_catalog_quotes.py
+++ quotes_dab/create_catalog_quotes.py
@@ -2,8 +2,10 @@ import sys
 
 # path = "/Workspace/Users/d.martins@kigroup.de/.bundle/quotes_dab_bundle/dev/files"
 from os import path
+from pathlib import Path
 
-sys.path.append(path)
+path_2 = Path().cwd()
+sys.path.append(path_2)
 
 from quotes_dab.provide_config import sql_cmd_create_catalog, sql_cmd_create_schema
 
