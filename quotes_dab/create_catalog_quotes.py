efad7c5a92514284105b0f493c00a90fd37912a7
doug-pires <dpiresmartins@yahoo.com.br>
2023-11-06 16:15:46 +0000

    refactor: Job yaml add as parameters to check if runs

--- quotes_dab/create_catalog_quotes.py
+++ quotes_dab/create_catalog_quotes.py
@@ -1,19 +1,7 @@
 import sys
 
-# path = "/Workspace/Users/d.martins@kigroup.de/.bundle/quotes_dab_bundle/dev/files"
-# from os import path
-from pathlib import Path
-
-path_file = Path().cwd().parent.as_posix()
-path_file_parent = Path("/").as_posix()
-
-
-print("Path Obsolute Parent:", path_file)
-print("Path Obsolute Parents:", path_file_parent)
-
-
-sys.path.append(path_file_parent)
-
+bundle_src_path = sys.argv[1]
+sys.path.append(bundle_src_path)
 from quotes_dab.provide_config import sql_cmd_create_catalog, sql_cmd_create_schema
 
 # Create Catalog
